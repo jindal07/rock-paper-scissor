0ea176022fccf3ca580108d19130ab530e993e2b
jindal07 <121694647+jindal07@users.noreply.github.com>
2024-06-27 21:39:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,65 +1,85 @@
-var timerEl=document.querySelector(".timer");
-var startEl=document.querySelector(".start");
-var stopEl=document.querySelector(".stop");
-var resetEl=document.querySelector(".reset");
+var buttonEl=document.querySelectorAll("button");
+var winEl=document.querySelector(".win-status");
+var playerEl=document.querySelector(".player");
+var compEl=document.querySelector(".comp");
+var restartEl=document.querySelector(".restart");
 
-startEl.addEventListener("click",start);
-stopEl.addEventListener("click",stop);
-resetEl.addEventListener("click",reset);
+var playerPoints=0;
+var compPoints=0;
+compEl.innerHTML=compPoints;
+playerEl.innerHTML=playerPoints;
 
-var startTime=0;
-var elapsedTime=0;
-var timerInterval;
 
-document.addEventListener("keydown",function(event){
-    var keyVal=event.key;
-    if(keyVal==="Enter"){
-        start();
-    }
-    else if(keyVal==="Escape"){
-        reset();
-    }
-    else if(keyVal===" "){
-        stop();
-    }
+restartEl.addEventListener("click",function(){
+    playerPoints=0;
+    compPoints=0;
+    compEl.innerHTML=compPoints;
+    playerEl.innerHTML=playerPoints;
+    winEl.innerHTML="";
 })
 
 
+buttonEl.forEach(button=>{
+    button.addEventListener("click",function(){
+        var computerChoice=compChoice();
+        var playerChoice=button.id;
+        if(playerChoice==="rock" && computerChoice==="scissor"){
+            winEl.innerHTML="You won🥳";
+            playerPoints++;
+            playerEl.innerHTML=playerPoints;
+        }
+        else if(playerChoice==="rock" && computerChoice==="paper"){
+            winEl.innerHTML="You lost😔";
+            compPoints++;
+            compEl.innerHTML=compPoints;
+        }
+        else if(playerChoice==="rock" && computerChoice==="rock"){
+            winEl.innerHTML="Its a tie!😀";
+            compPoints++;
+            playerPoints++;
+            compEl.innerHTML=compPoints;
+            playerEl.innerHTML=playerPoints;
+        }
+        if(playerChoice==="paper" && computerChoice==="rock"){
+            winEl.innerHTML="You won";
+            playerPoints++;
+            playerEl.innerHTML=playerPoints;
+        }
+        else if(playerChoice==="paper" && computerChoice==="scissor"){
+            winEl.innerHTML="You lost";
+            compPoints++;
+            compEl.innerHTML=compPoints;
+        }
+        else if(playerChoice==="paper" && computerChoice==="paper"){
+            winEl.innerHTML="Its a tie!";
+            compPoints++;
+            playerPoints++;
+            compEl.innerHTML=compPoints;
+            playerEl.innerHTML=playerPoints;
+        }
 
-function start(){
-     startTime=Date.now() -elapsedTime;
-     timerInterval=setInterval(function(){
-        elapsedTime=Date.now() - startTime;
-        timerEl.textContent=formatTime(elapsedTime);
+        if(playerChoice==="scissor" && computerChoice==="paper"){
+            winEl.innerHTML="You won";
+            playerPoints++;
+            playerEl.innerHTML=playerPoints;
+        }
+        else if(playerChoice==="scissor" && computerChoice==="rock"){
+            winEl.innerHTML="You lost";
+            compPoints++;
+            compEl.innerHTML=compPoints;
+        }
+        else if(playerChoice==="scissor" && computerChoice==="scissor"){
+            winEl.innerHTML="Its a tie!";
+            compPoints++;
+            playerPoints++;
+            compEl.innerHTML=compPoints;
+            playerEl.innerHTML=playerPoints;
+        }
+    })
+})
 
-        startEl.disabled=true;
-        stopEl.disabled=false;
-     },10);
-}
-
-function formatTime(elapsedTime){
-    var hours=Math.floor(elapsedTime/3600000);
-    var minutes=Math.floor((elapsedTime%3600000)/60000);
-    var seconds=Math.floor((elapsedTime%60000)/1000);
-    var milliseconds=Math.floor((elapsedTime%1000)/10);
-    return(
-        (hours<10?"0"+hours:hours)+":" +
-        (minutes<10?"0"+minutes:minutes)+":" +
-        (seconds<10?"0"+seconds:seconds)+"." +
-        (milliseconds<10?"0"+milliseconds:milliseconds)
-    )
-}
-
-function stop(){
-   clearInterval(timerInterval);
-   startEl.disabled=false;
-   stopEl.disabled=true;
-}
-
-function reset(){
-    clearInterval(timerInterval);
-    elapsedTime=0;
-    timerEl.textContent="00:00:00";
-    startEl.disabled=false;
-    stopEl.disabled=true;
+function compChoice(){
+    var choices=["rock","paper","scissor"];
+    var randomChoice=Math.floor(Math.random()*choices.length)
+    return choices[randomChoice];
 }
